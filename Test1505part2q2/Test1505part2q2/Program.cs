88b5d74c7ec20f74c88de54c85d991cc02ed2e90
yonatan2019 <47153587+yonatan2019@users.noreply.github.com>
2019-05-15 20:05:40 +0300

    Add files via upload

---
+++ Test1505part2q2/Test1505part2q2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1505part2q2
+{
+    class MyGenericClass<T>
+    {
+        List<T> list = new List<T>();
+
+        public MyGenericClass()
+        {
+
+        }
+
+        public bool Add(T item)
+        {
+            if (!list.Contains(item))
+            {
+                list.Add(item);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Remove(T item)
+        {
+            if (list.Contains(item))
+            {
+                list.Remove(item);
+                return true;
+            }
+
+            return false;
+        }
+
+        public T Peek(int index)
+        {
+            return list[index];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return this.list[index];
+            }
+            set
+            {
+                if (Equals(list[index], value))
+                    return;
+                if (list.Contains(value))
+                    return;
+                list[index] = value;
+            }
+        }
+    }
+}
